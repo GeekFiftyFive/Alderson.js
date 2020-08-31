79fa3ad6e72f2130743ab9c78605c9e5f2964e17
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-08-31 10:04:13 +0100

    Add additional config validation test

--- __tests__/validation/ConfigValidator.test.ts
+++ __tests__/validation/ConfigValidator.test.ts
@@ -3,9 +3,15 @@ import { MockConfigBuilder } from "../../__mocks__/validation/mockConfigs";
 import { validate } from "../../src/validation/ConfigValidator"
 
 describe("ConfigValidator", () => {
-    it("should pass when a config with just endpoints is used", () => {
+    it("should validate when a config with just endpoints is used", () => {
         let config: Config = new MockConfigBuilder().withEndpoints().build();
         const errors = validate(config);
         expect(errors).toHaveLength(0);
     });
+
+    it("should validate when a config with just actions is used", () => {
+        let config: Config = new MockConfigBuilder().withActions().build();
+        const errors = validate(config);
+        expect(errors).toHaveLength(0);
+    })
 });
